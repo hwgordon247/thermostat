cf2150e31f2b760ff7937573cb3a5cdddcdf304c
Henry Gordon <hwgordon247@gmail.com>
2016-02-17 16:10:58 +0000

    useing weather api and can pick a city

--- jasmine/js/interface.js
+++ jasmine/js/interface.js
@@ -34,4 +34,12 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.colour());
 
   };
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  });
 });
