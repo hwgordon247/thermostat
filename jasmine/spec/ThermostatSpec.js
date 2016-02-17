797cfe70a7bdb2a9f0721388f895a350e51b0b3a
Heather <campbell.hf@gmail.com>
2016-02-17 11:23:26 +0000

    increase and decreases temperature by 1

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -1,10 +1,22 @@
 describe("Thermostat", function() {
   var thermostat;
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
+
   it("starts at 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20)
+  });
+
+  it("increases by 1 degress when commanded to", function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21)
+  });
+
+  it("decreases by 1 degress when commanded to", function() {
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19)
   });
 
 });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -2,3 +2,11 @@ function Thermostat() {
   this.DEFAULTTEMPERATURE = 20;
   this.temperature = this.DEFAULTTEMPERATURE ;
 };
+
+  Thermostat.prototype.up = function(){
+    this.temperature ++;
+  };
+
+  Thermostat.prototype.down = function(){
+    this.temperature --;
+  };
